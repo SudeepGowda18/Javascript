1b9d52ebe261c192d2f7c1ec169572e583025937
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-06-10 07:55:12 +0000

    config done

---
+++ Basics/datatype.js
@@ -0,0 +1,13 @@
+//number => 2 to power 54
+//String => ""
+//boolean => true/false
+//BigInt
+//null => standalone Value
+//undefined => 
+//symbol => unique
+
+
+
+console.log(typeof 5)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
---
+++ Basics/variables.js
@@ -0,0 +1,9 @@
+const num = 123456789
+let name = "Sudeep"
+var lastname = "Gowda"
+let clgName
+
+// num = 1524 //Not Allowed
+name = "Kotresh" // Allowed
+console.log(lastname)
+console.table([num,name,lastname, clgName])
