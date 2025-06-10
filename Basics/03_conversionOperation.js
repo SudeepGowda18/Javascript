b67663a4aaa67fe178847af2a18dc1bda2bded25
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-06-10 17:28:16 +0000

    Conversion of Datatypes

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -1,3 +1,22 @@
-let score = 35
+let score = "35Sudeep"
 console.log(typeof (score))
 console.log(typeof score)
+
+let valueNum = Number(score)
+console.log(typeof valueNum)
+console.log(valueNum)
+
+let isLoggedIn =  "Sudeep"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// "" => false
+// "Sudeep" => true
+// 1 => true
+// 0 => false
+
+let stringValue = 55
+let stringConvertValue = String(stringValue)
+console.log(stringValue);
+console.log(typeof stringConvertValue);
