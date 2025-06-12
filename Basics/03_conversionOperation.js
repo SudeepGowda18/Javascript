a9228f71a2564c0f877f489319830627c837db51
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-06-12 06:27:23 +0000

    Operations

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -1,15 +1,15 @@
 let score = "35Sudeep"
-console.log(typeof (score))
-console.log(typeof score)
+// console.log(typeof (score))
+// console.log(typeof score)
 
 let valueNum = Number(score)
-console.log(typeof valueNum)
-console.log(valueNum)
+// console.log(typeof valueNum)
+// console.log(valueNum)
 
 let isLoggedIn =  "Sudeep"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
 // "" => false
 // "Sudeep" => true
@@ -18,5 +18,22 @@ console.log(typeof booleanIsLoggedIn);
 
 let stringValue = 55
 let stringConvertValue = String(stringValue)
-console.log(stringValue);
-console.log(typeof stringConvertValue);
+// console.log(stringValue);
+// console.log(typeof stringConvertValue);
+
+//*********OPERATIONS*********/
+
+let value = 5
+let negValue = -value
+console.log(negValue);
+console.log(2*3);
+console.log(2**3);
+console.log("1" + 2 + 10);
+console.log("1"+"4");
+console.log(-"");
+
+let game = 100
+++game
+console.log(game);
+
+
