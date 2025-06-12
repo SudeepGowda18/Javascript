bf8acfcdf0298736c42c7e66aa2be17ef65cce0b
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-06-12 07:42:08 +0000

    Changes

--- Basics/04_datatypes.js
+++ Basics/04_datatypes.js
@@ -16,3 +16,17 @@ console.log(id === anotherid);
 const myFunx = function() {
     console.log("Hello There");
 }
+
+let userOne = {
+    email : "sudeep@gmail.com",
+    upi : "456%vtg"
+}
+
+let usertwo = userOne;
+
+usertwo.email = "sudeep2ndmail"
+
+console.log(userOne.email);
+console.log(usertwo.email);
+
+console.log(userOne);
