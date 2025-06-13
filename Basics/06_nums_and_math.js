9ced0402acde62b234cfb5130885e2f00354aeeb
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-06-13 07:19:27 +0000

    math and num

---
+++ Basics/06_nums_and_math.js
@@ -0,0 +1,17 @@
+const score = 400
+console.log(score);
+
+const bal = new Number(500)
+console.log(bal.toFixed(3));
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Math.abs(-456));
+// console.log(Math.round(4.65));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,5,7,8,9));
+
+console.log(Math.floor(Math.random()*(6-1 + 1) + 1));
+ 
