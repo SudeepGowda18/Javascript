d18134be7f4ef247c1ebf69d5ebf6db1effc8426
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-06-13 06:12:58 +0000

    Strings

---
+++ Basics/05_Strings.js
@@ -0,0 +1,14 @@
+const name = "Sudeep"
+const fullname = 'Sudeep' + "gowda"
+const repo = 30
+console.log(`my name is ${name} and my fullname is ${fullname} and my repo is ${repo}`);
+console.log(fullname);
+const gameName = new String('Pubgunknownbattleground')
+console.log(gameName[0]);
+console.log(gameName[4]);
+console.log(gameName.toUpperCase());
+console.log(gameName.substring(0,4));
+
+const newString = "     Su de ep   "
+console.log(newString.trim());
+console.log(newString.replaceAll('e','E'));
