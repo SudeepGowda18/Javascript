7a9820c98cb2879ca798fec3f05c92b94b983d01
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-06-15 10:35:25 +0000

    arrays part 1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,29 @@
+const array = [1,2,3,4,5]
+// console.log(array);
+
+// array.push(7)
+// console.log(array);
+
+// array.pop()
+// console.log(array);
+
+// array.unshift(99)
+// array.shift()
+// console.log(array);
+
+// array.pop()
+// console.log(array);
+
+//console.log(array.includes(55));
+//console.log(array.indexOf(5));
+
+// const newArr = array.join()
+// console.log(array);
+// console.log(newArr);
+
+const newarr1 = array.slice(1,3)
+
+console.log(newarr1);
+
+const newarr2 = array.splice(1,3)
+console.log(newarr2);
