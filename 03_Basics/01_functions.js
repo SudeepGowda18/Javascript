d0ea02ed2da65277e79bff656759701f766fdd86
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-06-17 17:32:15 +0000

    objects

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -26,4 +26,34 @@ function loginUserMessage(username = "Sudi"){
 }
 
 // console.log(loginUserMessage());
-console.log(loginUserMessage("Sammy"));
+// console.log(loginUserMessage("Sammy"));
+
+function calculateCartPrice(...num){
+    return num
+}
+
+// console.log(calculateCartPrice(100,200,300,400));
+
+const user = {
+    username : "Sudeep",
+    price : 1000
+}
+
+function handleObject(anyObject){
+    // console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sudeep",
+    price:1500
+})
+
+const mynewarray = [200,400,600]
+
+function returnSecondVal(getarray){
+    return getarray[1]
+}
+
+// console.log(returnSecondVal(mynewarray));
+console.log(returnSecondVal([500,1000,1500]));
