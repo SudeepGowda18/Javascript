0adfa7393cd5f1541f7d28c05d0e12db2f29707b
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-06-18 15:34:50 +0000

    objects 2

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -3,9 +3,20 @@ if(true){
 let a = 10
 const b = 20
 // var c = 30
-console.log("Inner: " + a);
+// console.log("Inner: " + a);
 
 }
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = 'Sudeep'
+    function two(){
+    const website = "youtube"
+    console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
