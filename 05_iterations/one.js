a1686b4ebfd4dbc3681099f46b67b491a9e8a017
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-06-20 17:18:22 +0000

    do while

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -12,3 +12,12 @@ for (let index = 0; index < array.length; index++) {
     console.log(element);
     
 }
+
+let score = 11
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while (score <= 10) {
+    
+}
